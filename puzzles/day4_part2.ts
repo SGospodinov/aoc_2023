b6dc2d12bd151895a77eff2e40c15cc6e409d9f3
Stanislav Gospodinov <SGospodinov@users.noreply.github.com>
2023-12-07 10:35:31 +0000

    AoC 2023: Day 4 - Optimise part 2

--- puzzles/day4_part2.ts
+++ puzzles/day4_part2.ts
@@ -5,34 +5,39 @@ interface Card {
   winningNumbers: number[],
   playerNumbers: number[],
   copies: number,
+  matchingNumbers: number,
 };
 
 const parseNumList = (numbersStr: string): number[] => numbersStr.split(/\s+/).map(Number);
+
+const getMatchingNumbers = (winningNumbers: number[], playerNumbers: number[]): number => {
+  const numbersSet = new Set(winningNumbers);
+  playerNumbers.forEach((n) => numbersSet.add(n));
+
+  return playerNumbers.length - (numbersSet.size - winningNumbers.length);
+};
 
 const parseCard = (cardStr: string): Card => {
   const [idStr, numbers] = cardStr.split(': ');
   const [winningNumsStr, playerNumStr] = numbers.split(' | ');
 
+  const winningNumbers = parseNumList(winningNumsStr);
+  const playerNumbers = parseNumList(playerNumStr);
+
   return {
     id: Number(idStr.substring("Card ".length)),
-    winningNumbers: parseNumList(winningNumsStr),
-    playerNumbers: parseNumList(playerNumStr),
+    winningNumbers,
+    playerNumbers,
     copies: 1,
+    matchingNumbers: getMatchingNumbers(winningNumbers, playerNumbers),
   };
-};
-
-const matchingNumbers = (card: Card): number => {
-  const numbersSet = new Set(card.winningNumbers);
-  card.playerNumbers.forEach((n) => numbersSet.add(n));
-
-  return card.playerNumbers.length - (numbersSet.size - card.winningNumbers.length);
 };
 
 const cards = (await inputLines(4)).map(parseCard);
 
 cards.forEach((card, index, cards) => {
   for (let j = 0; j < card.copies; j++) {
-    for (let i = 1; i <= matchingNumbers(card); i++) {
+    for (let i = 1; i <= card.matchingNumbers; i++) {
       cards[index + i].copies += 1;
     };
   };
